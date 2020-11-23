db5590faaa29faeda6324da94c24399bf1f637cf
bachchanvinh <vinhbach19@gmail.com>
2020-11-23 11:18:39 +0700

    update CSS

--- src/Components/Todo/Edit.js
+++ src/Components/Todo/Edit.js
@@ -1,5 +1,5 @@
 export function Edit (props){
     return (
-        <textarea type ="text" onChange={props.onChange} value={props.value} onKeyDown={props.Enter} style={{display:`${props.display}`}}ref={props.autoFocus}></textarea>
+        <textarea autoFocus="true" className="Edit" type ="text" onChange={props.onChange} value={props.value} onKeyDown={props.Enter} style={{display:`${props.display}`}}ref={props.autoFocus}></textarea>
     )
 }
