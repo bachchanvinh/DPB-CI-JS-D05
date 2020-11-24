46536511e69e1c3269bdee474f70e4756c4716a1
bachchanvinh <vinhbach19@gmail.com>
2020-11-24 13:27:55 +0700

    weatherapp

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
 import React from 'react'
 import './App.css';
-import {Header} from './Component/Header'
-import {Weatherlist} from './Component/Weatherlist'
+import { Header } from './Component/Header'
+import { Weatherlist } from './Component/Weatherlist'
 const apiKey = "4d8fb5b93d4af21d66a2948710284366";
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-      weatherlist:[]
+    this.state = {
+      weatherlist: []
     }
-    this.handleKeydown=this.handleKeydown.bind(this)
+    this.handleKeydown = this.handleKeydown.bind(this)
   }
-  getData(inputVal){
-    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`).then(res=> res.json())
+  getData(inputVal) {
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`).then(res => res.json())
   }
-  handleKeydown(e){
+  handleKeydown(e) {
     this.getData(e.target.parentElement.children[0].value)
-    .then(data=> {
-      console.log(data)
-    let weather = <Weatherlist cityname={data.name} cel={data.main.temp} icon={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-     data.weather[0]["icon"]}.svg`}description={data.weather[0].description}/>
-    let list =this.state.weatherlist
-    list.push(weather)
-    this.setState({weatherlist:list})
-    console.log(this.state.weatherlist)
-  })
+      .then(data => {
+        console.log(data)
+        let weather = <Weatherlist cityname={data.name} cel={data.main.temp} icon={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.weather[0]["icon"]}.svg`} description={data.weather[0].description} />
+        let list = this.state.weatherlist
+        list.push(weather)
+        this.setState({ weatherlist: list })
+        console.log(this.state.weatherlist)
+      })
   }
-  render(){
-  return (
-    <div className="App">
-    <Header onClick={this.handleKeydown}/>
-    <div className="list">
-    {this.state.weatherlist.map(x=>x)}
-    </div>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Header onClick={this.handleKeydown} />
+        <div className="list">
+          {this.state.weatherlist.map(x => x)}
+        </div>
+      </div>
+    );
   }
 }
 
