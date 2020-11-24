49c7194463be1cc2a60a2554bb623a8b63975171
bachchanvinh <vinhbach19@gmail.com>
2020-11-24 13:33:22 +0700

    Weatherapp

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      weatherlist: []
+      weatherlist: [],
+      error:""
     }
     this.handleKeydown = this.handleKeydown.bind(this)
   }
@@ -17,20 +18,23 @@ class App extends React.Component {
   handleKeydown(e) {
     this.getData(e.target.parentElement.children[0].value)
       .then(data => {
+        this.setState({error:""})
         console.log(data)
         let weather = <Weatherlist cityname={data.name} cel={data.main.temp} icon={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.weather[0]["icon"]}.svg`} description={data.weather[0].description} />
         let list = this.state.weatherlist
         list.push(weather)
         this.setState({ weatherlist: list })
         console.log(this.state.weatherlist)
-      })
+      }).catch(()=>{this.setState({error:"Invald city"})}
+      )
   }
   render() {
     return (
       <div className="App">
         <Header onClick={this.handleKeydown} />
+        {this.state.error}
         <div className="list">
-          {this.state.weatherlist.map(x => x)}
+          {this.state.weatherlist}
         </div>
       </div>
     );
