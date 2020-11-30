e1477c42943b48434d1c236f6afab585a2893b10
bachchanvinh <vinhbach19@gmail.com>
2020-11-30 19:11:14 +0700

    update CSS button

--- src/Components/Todo/Todolist.js
+++ src/Components/Todo/Todolist.js
@@ -1,12 +1,11 @@
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+
 export function Todolist(props) {
     return (
         <div className="aaa">
         <p><span className={props.className}>{props.todo} </span>
         <span className="buttons">
-        <span className="button" onClick={props.onClickedit}>< EditIcon/></span>
-        <span className="button" onClick={props.onClickX}><DeleteIcon/></span></span></p>
+        <span className="button" onClick={props.onClickedit}>🖊️</span>
+        <span className="button" onClick={props.onClickX}>🗑️</span></span></p>
         </div>
     )
 }
